7a1ec138ee46d03101d0d22f551731e7b3c36ad5
xpolis03 <43518145+xpolis03@users.noreply.github.com>
2019-02-23 14:52:21 +0100

    [KP] -> added simple gui - tabs

--- src/IJAProject.java
+++ src/IJAProject.java
@@ -1,10 +1,16 @@
 import Abstracts.ChessPiece;
-import Board.Board;
-import Enums.Color;
-import Figures.*;
 
+//import Enums.Color;
+import Figures.King;
+import Figures.Movement;
+
+import java.awt.*;
 import java.util.ArrayList;
 import java.util.List;
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.lang.String;
 
 public class IJAProject {
 
@@ -18,20 +24,76 @@ public class IJAProject {
          * Call move function with new position parameters
          * Render frontend when it is done
          */
+
+        JFrame frame= new JFrame("IJA Project");
+        frame.setSize(1000,850);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setLayout(new FlowLayout(FlowLayout.LEFT));
+
+        Font font = new Font("Verdana", Font.PLAIN, 14);
+
+
+        /*Menu*/
+        JMenuBar menuBar = new JMenuBar();
+        JMenu menuGame = new JMenu("Game");
+        menuGame.setFont(font);
+
+        JMenuItem newGame = new JMenuItem("New");
+        newGame.setFont(font);
+        menuGame.add(newGame);
+
+        JMenuItem refreshGame = new JMenuItem("Refresh");
+        menuGame.add(refreshGame);
+        refreshGame.setFont(font);
+
+        JMenuItem exitGame = new JMenuItem("Exit");
+        exitGame.setFont(font);
+        menuGame.add(exitGame);
+
+        exitGame.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                System.exit(0);
+            }
+        });
+
+
+        JMenu menuActions = new JMenu("Actions");
+        menuActions.setFont(font);
+        JMenuItem unDo = new JMenuItem("Undo");
+        unDo.setFont(font);
+        menuActions.add(unDo);
+        JMenuItem reDo = new JMenuItem("Redo");
+        reDo.setFont(font);
+        menuActions.add(reDo);
+
+        JMenu menuView = new JMenu("View");
+        menuView.setFont(font);
+
+        frame.setJMenuBar(menuBar);
+        menuBar.add(menuGame);
+        menuBar.add(menuActions);
+        menuBar.add(menuView);
+
+        // Makes new Tab
+        Tab firstTab = new Tab();
+        firstTab.addNewTab(frame, "Game 1");
+        firstTab.addNewTab(frame, "Game 2");
+
+        frame.setVisible(true);
+
+
+
         Game chessGame = new Game();
 
-        chessGame.setPiece(new Pawn(Color.BLACK), 1, 0);
-        chessGame.setPiece(new Pawn(Color.BLACK), 0, 1);
-        chessGame.setPiece(new Pawn(Color.BLACK), 1, 1);
-
-        chessGame.setPiece(new King(Color.BLACK), 0, 0);
+        /** TODO seems as fuck. refactor it. Coordinates will be set right from Cell */
+        chessGame.setPiece(new King(Enums.Color.BLACK), 0, 0);
 
         // Simulate mouse button click. Now we have a chess piece from selected cell
         ChessPiece boardPiece = chessGame.getBoardPiece(0, 0);
 
         // Get all possible statements of current piece
         List<Movement> possibleMovements = new ArrayList<>(boardPiece.calculatePossibleMovements()) ;
-        chessGame.isPossible(possibleMovements, boardPiece.getColor());
+//        chessGame.isPossible(possibleMovements, boardPiece.getColor());
 
 //        // Remove moves from possible moves for currently selected chess piece which couldn't be done
 //        for(int counter = 0; counter < possibleMovements.size(); counter++){
